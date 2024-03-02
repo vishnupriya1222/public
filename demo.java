2f244969750254c2c41f9960b259c3d158418b8f
vishnupriya1222 <guntimaduguvishnupriya3@gmail.com>
2024-03-02 07:05:13 +0000

    added demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 class demo {
    public static void main(String[] args) {
-           system.out.println("Hello All) ;
+           
 
 	   system.out.println("Hello friends");
 
@@ -8,7 +8,7 @@ class demo {
 	   system.out.println("Hello guys");
 
 
-	system.out.println("Hello priya")  ;
+	
 
    }
 }
